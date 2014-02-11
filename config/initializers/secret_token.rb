3682e1cff598381922f83bfd35698637cb73161f
chie voeltz <cvoeltz@madisoncollege.edu>
2014-02-10 19:28:15 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookmarkSharingSiteApp::Application.config.secret_key_base = '5b9d12c388455fccc91e4298e204fe3aae567221829536f4031bbaf48c00499d893d3038c755c20bf225b907f8d25dba7880d3d3b64de79c229233a643180906'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BookmarkSharingSiteApp::Application.config.session_store :cookie_store, key: '_bookmark_sharing_site_app_session'
