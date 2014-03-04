dc8508a22bf4afe5f81a726c72f9911168c3b649
chie voeltz <chonda39@gmail.com>
2014-03-03 21:29:07 -0600

    fixed the bookmarks error”

--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -23,8 +23,9 @@ describe BookmarksController do
   # This should return the minimal set of attributes required to create a valid
   # Bookmark. As you add validations to Bookmark, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "url" => "MyString" } }
-
+  #let(:valid_attributes) { { "url" => "MyString" } }
+  let(:valid_attributes) {{url: "www.google.com", name: "bob"}}
+  
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # BookmarksController. Be sure to keep this updated too.
